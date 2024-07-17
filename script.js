4ba1b2de04e7b4291d7e239938f75d11ac9b00e3
Trond Sletten <trond-sletten@hotmail.com>
2024-07-17 08:29:48 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let lapCount = 0
+let lapEl = document.getElementById("lap")
+let saveEl = document.getElementById("saved")
+
+function count (){
+    lapCount += 1
+    lapEl.textContent = lapCount
+}
+function save (){
+    let displayLaps = lapCount + " - "
+    saveEl.textContent += displayLaps 
+    lapCount = 0
+    lapEl.textContent = lapCount
+}
+
+function reset () {
+    lapCount = 0
+    saveEl.textContent = ""
+    lapEl.textContent = 0
+}
